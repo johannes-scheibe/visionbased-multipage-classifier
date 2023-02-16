66b1f79aba6f1e0fa76e949faccd2ba042cd2d4c
Johannes Scheibe <scheibe.johannes@t-online.de>
2023-02-16 11:04:33 +0100

    Implement swin encoder and image preprocessor

---
+++ multipage_classifier/dataset.py
@@ -0,0 +1,168 @@
+import json
+import math
+import os
+
+from pathlib import Path
+import random
+from typing import Dict, List
+
+import pytorch_lightning as pl
+import torch
+from torch.utils.data import DataLoader, Dataset, default_collate
+
+from PIL import Image
+
+class UCSFDataset(Dataset):
+    def __init__(
+        self,
+        dataset_path: Path,
+        labels: List[dict],
+        prepare_function
+    ):
+        super().__init__()
+        self.dataset_path = dataset_path
+        self.labels = labels
+        
+        self.prepare_function = prepare_function
+
+        self.classes = []
+        for l in self.labels:
+            if l["type"] not in self.classes:
+                self.classes.append(type)
+
+        self.id2class = {idx: str(label) for idx, label in enumerate(self.classes)}
+        self.class2id = {str(label): idx for idx, label in enumerate(self.classes)}
+        
+        self.dataset_length = len(self.labels)
+
+    def __len__(self):
+        return self.dataset_length
+
+    def __getitem__(self, idx: int):
+        
+        label = self.labels[idx]
+
+        img_folder = label["image_folder"]
+
+        pages = []
+        for i, file in enumerate(os.listdir(self.dataset_path / img_folder)):
+            path = self.dataset_path / img_folder / file
+            pages.append(
+                {
+                    "pixel_values": self.prepare_function(Image.open(path)),
+                    "letter_id": label["id"], # we can set this to the doc_id because its unique as well 
+                    "doc_class":  label["type"],
+                    "doc_id": label["id"], 
+                    "page_nr": i
+                }
+            )
+        return pages
+
+
+
+class UCSFDataModule(pl.LightningDataModule):
+    def __init__(self, dataset_path, prepare_function, split: list = [0.8, 0.2], batch_size=8, num_workers: int = 1):
+        super().__init__()
+        self.dataset_path = dataset_path
+        self.prepare_function = prepare_function
+
+        assert math.fsum(split) == 1.0, f"{split} doesn't add up to 1 ({math.fsum(split)})"
+        assert len(split) in [2, 3], f"Length of {split} is not in [2, 3] ({len(split)})"
+
+        self.split = split
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+
+    def prepare_data(self):
+        labels = json.load(open(self.dataset_path / "labels.json"))
+
+        random.shuffle(labels)
+
+        n = len(labels)
+        sizes = [int(n * p) for p in self.split]
+        slices = []
+        start = 0
+        for size in sizes:
+            end = start + size
+            slices.append(labels[start:end])
+            start = end
+
+        self.train_labels = slices[0]
+        self.val_labels = slices[1]
+        self.test_labels = slices[2] if len(slices) == 3 else slices[1]
+
+            
+    def setup(self, stage=None):
+        self.train_dataset = UCSFDataset(
+            self.dataset_path, self.train_labels, self.prepare_function
+        )
+        self.val_dataset = UCSFDataset(
+            self.dataset_path, self.val_labels, self.prepare_function
+        )
+        self.test_dataset = UCSFDataset(
+            self.dataset_path, self.test_labels, self.prepare_function
+        )
+
+    def train_dataloader(self):
+        return DataLoader(
+            self.train_dataset,
+            batch_size=self.batch_size,
+            shuffle=True,
+            num_workers=self.num_workers,
+            pin_memory=True,
+            collate_fn=collate,
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            self.val_dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+            pin_memory=True,
+            collate_fn=val_collate,
+        )
+
+    def test_dataloader(self):
+        return DataLoader(
+            self.test_dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+            pin_memory=True,
+            collate_fn=val_collate,
+        )
+
+
+def collate(samples: List[List[dict]], shuffle_mode="window"): # "all", "none"
+    prep_samples: List[dict] = []
+    for sample in samples:  
+        if shuffle_mode == "all":
+            random.shuffle(sample)
+        elif shuffle_mode == "window":
+            window_shuffle(sample)
+        prep_samples.extend(sample)
+
+    batch = [sample for sample in prep_samples]
+    ret = default_collate(batch)
+    return ret
+
+def val_collate(samples: List[List[dict]]) -> Dict:
+    prep_samples: List[dict] = []
+    for sample in samples:
+        prep_samples.extend(sample)
+    batch = [sample for sample in prep_samples]
+
+    # Using the whole batch could use too much memory. Monitor this.
+    ret = default_collate(batch)
+
+    return ret
+
+def window_shuffle(input_list, window_size=3, shuffle_percent=0.25):
+    shuffle_idxs = random.sample(
+        range(0, len(input_list) - window_size, 1), k=int(shuffle_percent * len(input_list))
+    )  # Possibly shuffles 25% of the input sequence with their 'window_size' neighbors
+    for i in shuffle_idxs:
+        random.shuffle(input_list[i : i + window_size])
+
+    return input_list
---
+++ multipage_classifier/encoder.py
@@ -0,0 +1,122 @@
+import warnings
+from inspect import signature
+from typing import Any, Dict, List, Optional
+
+import numpy as np
+import pytorch_lightning as pl
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from transformers import AdamW, AutoModel
+
+ORDER_NAMES = ["None", "Pred", "Succ", "Same"]
+
+class EncoderForEmbedding(pl.LightningModule):
+    encoder: nn.Module
+    metrics: torch.nn.ModuleDict
+    # confmat: torch.nn.ModuleDict
+    heads: torch.nn.ModuleDict
+    weights: torch.nn.ParameterDict
+
+    def __init__(self, encoder: nn.Module, hidden_dim):
+        super().__init__()
+        self.save_hyperparameters()
+
+        self.encoder = encoder
+
+        self.hidden_dim = hidden_dim
+        self.model_input_keys = list(signature(self.encoder.forward).parameters.keys())
+
+        # create order head
+        self.heads = torch.nn.ModuleDict({})
+        self.heads["order"] = torch.nn.Linear(self.hidden_dim * 2, len(ORDER_NAMES))
+        self.weights = torch.nn.ParameterDict({})
+        self.weights["order"] = torch.nn.parameter.Parameter(
+            data=torch.Tensor([0.1, 1, 1, 1]).float(), requires_grad=False
+        )
+
+        self.warned_nan = False
+
+        # self.dropout = (
+        #     torch.nn.Dropout(self.config.dropout)
+        #     if self.config.dropout is not None
+        #     else torch.nn.Identity()
+        # )
+
+    def forward(self, batch, **kwargs) -> torch.Tensor:
+        model_input = {k: v for k, v in batch.items() if k in self.model_input_keys}
+        cls_embeddings = self.encoder(**model_input)["last_hidden_state"]#[:, 0, :]  # [batch_size, seq_len, hidden_size]
+
+        return cls_embeddings
+
+    def step(self, batch: Any):
+        emb = self.forward(batch)
+        print(emb.size())
+        bs = len(emb)
+
+        preds: Dict[str, torch.Tensor] = {}
+        ground_truth = {}
+
+        # Compute order head input
+        diff = (emb.unsqueeze(1) - emb.unsqueeze(0)).view(-1, self.hidden_dim)
+        diff = torch.cat([diff, diff], -1)
+        print(diff.size())
+        
+        preds["order"] = torch.log_softmax(self.heads["order"](diff), dim=-1)
+
+        same_doc = (batch["doc_id"].unsqueeze(1) == batch["doc_id"].unsqueeze(0)).int()
+        same_letter = (batch["letter_id"].unsqueeze(1) == batch["letter_id"].unsqueeze(0)).int()
+        less = (batch["page_nr"].unsqueeze(1) < batch["page_nr"].unsqueeze(0)).int() * 1
+        greater = (batch["page_nr"].unsqueeze(1) > batch["page_nr"].unsqueeze(0)).int() * 2
+        same = (batch["page_nr"].unsqueeze(1) == batch["page_nr"].unsqueeze(0)).int() * 3
+
+        ground_truth["order"] = (
+            ((less + greater + same) * same_doc * same_letter).long().view(-1)
+        )  # [ones]
+
+        return self.calc_metrics(preds, ground_truth)
+
+    def calc_metrics(self, preds, gt):
+        losses = {}
+        metrics = {}
+        res = []
+
+        for k, v in preds.items():
+            res.append(v)
+            losses[f"{k}_loss"] = torch.nn.NLLLoss(weight=self.weights[k])(v, gt[k])
+            metrics[f"{k}_acc"] = (v.argmax(dim=-1) == gt[k]).float().mean()
+
+        return (
+            losses,
+            metrics,
+            res,
+        )
+
+    def training_step(self, batch: Any, batch_idx: int):
+        losses, metrics, _ = self.step(batch)
+        metrics["loss"] = sum(losses.values())
+        metrics.update({(f"loss_{k}" if "loss" not in k else k): v for k, v in losses.items()})
+        self.log_dict(metrics)
+        return metrics
+
+    def validation_step(self, batch: Any, batch_idx: int):
+        losses, metrics, _ = self.step(batch)
+        metrics["val_loss"] = sum(losses.values())
+        metrics.update({(f"loss_{k}" if "loss" not in k else k): v for k, v in losses.items()})
+        self.log_dict(metrics)
+        return metrics
+
+    def test_step(self, batch: Any, batch_idx: int):
+        losses, metrics, _ = self.step(batch)
+        metrics["val_loss"] = sum(losses.values())
+        metrics.update({(f"loss_{k}" if "loss" not in k else k): v for k, v in losses.items()})
+        self.log_dict(metrics)
+        return metrics
+
+    def configure_optimizers(self):
+        optimizer = AdamW(self.parameters(), lr=2e-5)
+
+        return dict(
+            optimizer=optimizer,
+        )
